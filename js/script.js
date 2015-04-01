819641515673f947b99ddab44e8e52f3417352af
Vivien Ilett <vivien.ilett@gmail.com>
2015-04-01 17:01:54 -0400

    saaaaaaassssss

--- js/script.js
+++ js/script.js
@@ -1,49 +1,29 @@
 var svgParty = {};
 	
 	// get length of path //
-	var path = document.querySelector('path');
+	var path = document.querySelector('path.hello');
 	var length = path.getTotalLength();
 	console.log(length);
 
-	// loop through CSS to find keyframe //
-
-	function findThings(rule) {
-
-		// get stylesheets in variable 
-		var ss = document.styleSheets;
-
-		// loop through //
-		for ( var i=0; i < ss.length ; ++i ) {
-			// loop through rules //
-			for ( var j=0; j<ss[i].cssRules.length; ++j ) {
-
-				// find keyframe 
-				if (ss[i].cssRules[j].type == window.cssRule.KEYFRAMES_RULE && ss[i].cssRules[j].name == rule ) return ss[i].cssRules[j];
-			}
-		}
-		// if null
-		return null;
-	}
-
-	// replace with var length //
-	function replace(anim) {
-		// find keyframe //
-		var keyframes = findKeyframesRule(anim);
-
-		// remove existing rule //
-		keyframes.deleteRule('from');
-
-		// create new rule - var length //
-		keyframes.insertRule('from { stroke-dashoffset:' + length '; }' );
-		
-		// assign to thing 
-		$('path').style.webkitAnimationName = anim;
-	}
-
 	$(document).ready(function() {
-		// $('path').addClass('vivispizza');
-	});
 
-	$('button').on('click', function(){
-		
+		var animate = function(element, className, animation) {
+			var hello = $(element);
+			hello.attr('class', className + ' ' + animation);
+
+			hello.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+				hello.attr('class', className);
+			});
+
+			$('button').on('click', function() {
+				hello.attr('class',className + ' ' + animation);
+
+				hello.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+					hello.attr('class',className);
+				});
+			});
+		}
+
+		animate('path.hello', 'hello', 'draw');
+
 	});
