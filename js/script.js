193dba534ca1934ba452c8d2aa138efc542427c1
Vivien Ilett <vivien.ilett@gmail.com>
2015-04-01 22:54:06 -0400

    updated some comments

--- js/script.js
+++ js/script.js
@@ -1,29 +1,40 @@
-var svgParty = {};
+
 	
-	// get length of path //
-	var path = document.querySelector('path.hello');
-	var length = path.getTotalLength();
-	console.log(length);
+// get length of path //
+var path = document.querySelector('path.hello');
+var length = path.getTotalLength();
+console.log(length);
 
-	$(document).ready(function() {
+$(document).ready(function() {
 
-		var animate = function(element, className, animation) {
-			var hello = $(element);
-			hello.attr('class', className + ' ' + animation);
+	// add mixin generated class to element //
+	var animate = function(element, className, animation) {
 
+		// hold element in variable //
+		var hello = $(element);
+
+		// add animation to element //
+		hello.attr('class', className + ' ' + animation);
+
+		// wait for animation to end, remove class // 
+		hello.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+			hello.attr('class', className);
+		});
+
+		// regenerate animation on click //
+		$('button').on('click', function() {
+
+			// add animation to element //
+			hello.attr('class',className + ' ' + animation);
+
+			// wait for animation to end, remove class // 
 			hello.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-				hello.attr('class', className);
+				hello.attr('class',className);
 			});
+		});
+	}
 
-			$('button').on('click', function() {
-				hello.attr('class',className + ' ' + animation);
+	// run animation function //
+	animate('path.hello', 'hello', 'draw');
 
-				hello.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
-					hello.attr('class',className);
-				});
-			});
-		}
-
-		animate('path.hello', 'hello', 'draw');
-
-	});
+});
